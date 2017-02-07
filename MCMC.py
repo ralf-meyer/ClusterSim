2c516079f959771bd485cf5d1c6969c2906d1004
Ralf Meyer <meyer.ralf@yahoo.com>
2017-02-07 19:28:26 +0100

    add read_xyz

--- MCMC.py
+++ MCMC.py
@@ -1,8 +1,21 @@
- # Markov Chain Monte Carlo Methods
+# Markov Chain Monte Carlo Methods
+
+import numpy as _np
 
 class MetropolisHastings(object):
-    def __init__(self):
+    def __init__(self, cluster, method):
+        self.cluster = cluster
+        self.markov_chain = []
+
+    def step(self):
+        
+
+        # Generate a trial geometry
+	# Choose random atom:
+        ind1 = _np.random.randint(0, len(self.cluster.atoms))
 
 
-    def iterate(self):
+        # Calculate acceptance ratio
 
+        # Append the result to the Markov Chain
+        
--- input_classes.py
+++ input_classes.py
@@ -1,20 +1,49 @@
-
+import warnings
+import numpy as _np
 
 class Atom(object):
     def __init__(self, atom_type, xyz):
+        # Todo: use either string or numeric atom type 
+        # and do the convertion here
         self.atom_type = atom_type
         self.xyz = xyz
 
 class Cluster(object):
     def __init__(self):
-
+        self.atoms = []
 
     def calc_neighbors(self):
         neighbor_list = []
 
     def analyze(self):
         # Calculates the energy of the current geometry
+        pass
 
     def optimize(self):
         # Function that optimizes the current geometry
         # returns the optimized energy
+        pass
+
+    def read(self, filename):
+        if filename[-4:] == ".xyz":
+            self.read_xyz(filename)
+        else:
+            warnings.warn("Unknown file extension!")        
+
+    def read_xyz(self, filename):
+        with open(filename, "r") as f_in:
+            for i, line in enumerate(f_in):
+                if i == 0:
+                    # set counter for number of lines to be read
+                    counter = int(line)
+                    # Skip comment line
+                    next(f_in)
+                elif counter >= 0:
+                    sp = line.split()
+                    self.atoms.append(Atom(sp[0], _np.array([float(s) for s in sp[1:4]])))
+                    counter -= 1
+                if counter == 0:
+                    break
+
+     
+
