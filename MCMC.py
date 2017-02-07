d4783eec95a6e0142ceec32ee2d7b16d2c80a0f8
Ralf Meyer <meyer.ralf@yahoo.com>
2017-02-07 16:37:43 +0100

    added first files

---
+++ MCMC.py
@@ -0,0 +1,8 @@
+ # Markov Chain Monte Carlo Methods
+
+class MetropolisHastings(object):
+    def __init__(self):
+
+
+    def iterate(self):
+
---
+++ input_classes.py
@@ -0,0 +1,20 @@
+
+
+class Atom(object):
+    def __init__(self, atom_type, xyz):
+        self.atom_type = atom_type
+        self.xyz = xyz
+
+class Cluster(object):
+    def __init__(self):
+
+
+    def calc_neighbors(self):
+        neighbor_list = []
+
+    def analyze(self):
+        # Calculates the energy of the current geometry
+
+    def optimize(self):
+        # Function that optimizes the current geometry
+        # returns the optimized energy
